14165fb2cba90a3b0d53d1f4a11e1098994eebe4
Rko_kkw <8898785+wang-kaidd@user.noreply.gitee.com>
2023-11-26 21:53:21 +0800

    引入uni-ui、uni-request请求及拦截器的封装

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-//
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <view class="cart">cart</view>
+  <uni-badge :text="'11111'" type="primary" size="normal" />
 </template>
 
 <style lang="scss">
--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,25 +1,31 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
+
+const getData = async () => {
+  const res = await http<string[]>({
+    method: 'GET',
+    url: '',
+  })
+  console.log('res', res)
+}
 </script>
 
 <template>
   <view class="my">
     <view>会员信息：{{ memberStore.profile }}</view>
     <button
-      @tap="
-        memberStore.setProfile({
-          nickname: '黑马先锋',
-        })
-      "
+      @tap="memberStore.setProfile({ nickname: '黑马先锋', token: '12345' })"
       size="mini"
-      plain
+      :plain="true"
       type="primary"
     >
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData" size="mini" type="primary" plain>测试接口</button>
   </view>
 </template>
 
