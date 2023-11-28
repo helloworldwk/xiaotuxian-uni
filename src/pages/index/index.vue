bb47b34baf3e7096e01e937be9b4f82a78899e7c
c_wangkai7 <1811824503@qq.com>
2023-11-28 17:38:06 +0800

    feat:猜你喜欢组件-类型定义和组件列表渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -41,17 +41,24 @@ onLoad(() => {
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <!-- 自定义轮播图 -->
-  <XtxSwiper :list="bannerList" />
-  <!-- 分类 -->
-  <CategoryPanel :list="categoryList" />
-  <!-- 热门推荐 -->
-  <HotPanel :list="hotList" />
-  <view class="index">index</view>
+
+  <scroll-view scroll-y>
+    <!-- 自定义轮播图 -->
+    <XtxSwiper :list="bannerList" />
+    <!-- 分类 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推荐 -->
+    <HotPanel :list="hotList" />
+    <!-- 猜你喜欢 -->
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
   background-color: #f7f7f7;
 }
 </style>
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,6 @@
 import { http } from '@/utils/http'
-import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import type { BannerItem, CategoryItem, GuessItem, HotItem } from '@/types/home'
+import type { PageParams, PageResult } from '@/types/global'
 
 /**
  *  首页-广告区域-小程序
@@ -33,5 +34,19 @@ export const getHomeHotAPI = () => {
   return http<HotItem[]>({
     method: 'GET',
     url: '/home/hot/mutli',
+  })
+}
+
+/**
+ *  猜你喜欢-小程序
+ * @param integer integer 页码默认值1
+ * @param pageSize integer 页大小默认是10
+ * @returns
+ */
+export const getHomeGoodsGuessLikeAPI = (data?: PageParams) => {
+  return http<PageResult<GuessItem>>({
+    method: 'GET',
+    url: '/home/goods/guessLike',
+    data,
   })
 }
