0b4a2b3760a4bfd3f49924427d3e9b5fb1210cf2
Rko_kkw <8898785+wang-kaidd@user.noreply.gitee.com>
2023-11-29 23:13:56 +0800

    feat:首页骨架屏

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,6 +5,7 @@ import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
 import HotPanel from './components/HotPanel.vue'
+import PageSkeleton from './components/PageSkeleton.vue'
 
 import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import type { XtxGuessInstance } from '@/types/component'
@@ -32,10 +33,12 @@ const getHomeHotData = async () => {
 
   hotList.value = result
 }
-onLoad(() => {
-  getHomeBannerData()
-  getHomeCategoryData()
-  getHomeHotData()
+onLoad(async () => {
+  isLoading.value = true
+
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+
+  isLoading.value = false
 })
 
 const guessRef = ref<XtxGuessInstance>()
@@ -44,6 +47,8 @@ const scrolltolowerHandler = () => {
   guessRef.value?.getMore()
 }
 
+// 加载状态
+const isLoading = ref(false)
 // 下拉刷新状态(true为已触发, false为未触发)
 const isTriggered = ref(false)
 // 下拉刷新触发的函数
@@ -53,7 +58,12 @@ const refresherrefreshHandler = async () => {
   // 重置猜你喜欢组件的数据
   guessRef.value?.resetData()
   // 重新请求轮播图、分类、热门推荐
-  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData(), guessRef.value?.getMore()])
+  await Promise.all([
+    getHomeBannerData(),
+    getHomeCategoryData(),
+    getHomeHotData(),
+    guessRef.value?.getMore(),
+  ])
 
   isTriggered.value = false
 }
@@ -64,16 +74,26 @@ const refresherrefreshHandler = async () => {
   <CustomNavbar />
 
   <!-- 滚动容器 -->
-  <scroll-view scroll-y @scrolltolower="scrolltolowerHandler" @refresherrefresh="refresherrefreshHandler"
-    refresher-enabled :refresher-triggered="isTriggered">
-    <!-- 自定义轮播图 -->
-    <XtxSwiper :list="bannerList" />
-    <!-- 分类 -->
-    <CategoryPanel :list="categoryList" />
-    <!-- 热门推荐 -->
-    <HotPanel :list="hotList" />
-    <!-- 猜你喜欢 -->
-    <XtxGuess ref="guessRef" />
+  <scroll-view
+    scroll-y
+    @scrolltolower="scrolltolowerHandler"
+    @refresherrefresh="refresherrefreshHandler"
+    refresher-enabled
+    :refresher-triggered="isTriggered"
+  >
+    <!-- 骨架屏 -->
+    <PageSkeleton v-if="isLoading" />
+
+    <template v-else>
+      <!-- 自定义轮播图 -->
+      <XtxSwiper :list="bannerList" />
+      <!-- 分类 -->
+      <CategoryPanel :list="categoryList" />
+      <!-- 热门推荐 -->
+      <HotPanel :list="hotList" />
+      <!-- 猜你喜欢 -->
+      <XtxGuess ref="guessRef" />
+    </template>
   </scroll-view>
 </template>
 
