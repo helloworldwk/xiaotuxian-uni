2b6241bc29f7c99d527226e421828cf7f789c092
c_wangkai7 <1811824503@qq.com>
2023-12-12 17:13:46 +0800

    feat:填写订单-收货地址

--- src/pagesMember/address/address.vue
+++ src/pagesMember/address/address.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import { getMemberAddressAPI, deleteMemberAddressAPI } from '@/services/address'
 import type { AddressItem } from '@/types/address'
 import { onShow } from '@dcloudio/uni-app'
+import { useAddressStore } from '@/stores'
 
 // 获取地址列表数据
 const addressList = ref<AddressItem[]>([])
@@ -31,6 +32,16 @@ const onDeleteAddress = (id: string) => {
     },
   })
 }
+
+// 地址 行点击 修改订单页的默认地址
+const onChangeAddress = (item: AddressItem) => {
+  console.log(item)
+  // 修改store中的默认地址
+  const { changeSelectedAddress } = useAddressStore()
+  changeSelectedAddress(item)
+  // 返回上一页
+  uni.navigateBack()
+}
 </script>
 
 <template>
@@ -41,7 +52,7 @@ const onDeleteAddress = (id: string) => {
         <uni-swipe-action class="address-list">
           <!-- 收货地址项 -->
           <uni-swipe-action-item class="item" v-for="item in addressList" :key="item.id">
-            <view class="item-content">
+            <view class="item-content" @tap="() => onChangeAddress(item)">
               <view class="user">
                 {{ item.receiver }}
                 <text class="contact">{{ item.contact }}</text>
@@ -52,6 +63,7 @@ const onDeleteAddress = (id: string) => {
                 class="edit"
                 hover-class="none"
                 :url="`/pagesMember/address-form/address-form?id=${item.id}`"
+                @tap.stop="() => {}"
               >
                 修改
               </navigator>
