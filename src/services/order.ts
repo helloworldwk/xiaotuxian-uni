e9b2cd7548c99defec6a717b0566ddda2d26c922
c_wangkai7 <1811824503@qq.com>
2023-12-13 17:08:48 +0800

    feat:订单详情-订单状态渲染

--- src/services/order.ts
+++ src/services/order.ts
@@ -1,5 +1,5 @@
 import { http } from '@/utils/http'
-import type { OrderCreateParams, OrderPreResult } from '@/types/order'
+import type { OrderCreateParams, OrderPreResult, OrderResult } from '@/types/order'
 
 /**
  * 填写订单-获取预付订单
@@ -39,5 +39,17 @@ export const postMemberOrderAPI = (data: OrderCreateParams) => {
     method: 'POST',
     url: '/member/order',
     data,
+  })
+}
+
+/**
+ * 获取订单详情
+ * @param id 订单id
+ * @returns
+ */
+export const getMemberOrderByIdAPI = (id: string) => {
+  return http<OrderResult>({
+    method: 'GET',
+    url: `/member/order/${id}`,
   })
 }
